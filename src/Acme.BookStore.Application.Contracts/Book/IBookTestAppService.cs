aba11574b15b7df9449e37bcf7c283a842927d35
François Kreutz <fkreutz@inspirit-tech.com>
2020-04-20 19:37:54 +0200

    Add project files.

---
+++ src/Acme.BookStore.Application.Contracts/Book/IBookTestAppService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Acme.BookStore.Book
+{
+    public interface IBookTestAppService : IApplicationService
+    {
+        Task<bool> BookTestAsync();
+    }
+}
---
+++ src/Acme.BookStore.Application/Book/BookTestAppService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace Acme.BookStore.Book
+{
+    [AllowAnonymous]
+    public class BookTestAppService : ApplicationService, IBookTestAppService
+    {
+        public virtual async Task<bool> BookTestAsync()
+        {
+            return true;
+        }
+    }
+}
